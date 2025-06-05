2ec31a9d4e2e955c299460e255b4124c09e16a4a
UmaVenkatesh_Andavar <UmaVenkatesh.Andavarapu@dellteam.com>
2025-06-05 23:00:50 +0530

    setup Redux

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
+import { useSelector } from "react-redux";
+
 const Navbar = () => {
+  const user = useSelector((store) => store.user);
+
   return (
-    <>
-      <div className="navbar bg-base-300 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
-        </div>
-        <div className="flex gap-2">
-          <div className="dropdown dropdown-end mx-5">
+    <div className="navbar bg-base-300 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">DevTinder</a>
+      </div>
+
+      {user && (
+        <div className="flex items-center gap-3 mx-5">
+          <p className="text-sm font-medium">Welcome, {user.firstName}</p>
+
+          <div className="dropdown dropdown-end">
             <div
               tabIndex={0}
               role="button"
@@ -14,20 +21,18 @@ const Navbar = () => {
             >
               <div className="w-10 rounded-full">
                 <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  alt="User avatar"
+                  src={user.photoUrl}
                 />
               </div>
             </div>
+
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-40"
             >
               <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
+                <a>Profile</a>
               </li>
               <li>
                 <a>Settings</a>
@@ -38,8 +43,8 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
